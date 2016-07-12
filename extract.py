d4a0518cfc73fa0b781f3bae9627922816558adb
Yiping Deng <scottdeng@live.cn>
2016-07-12 17:20:38 +0800

    add write to file function

--- extract.py
+++ extract.py
@@ -3,25 +3,32 @@ import os, string, shutil, re, sys
 import pefile
 from pefile import DIRECTORY_ENTRY
 
-def main():
-    fname = sys.argv[1]
-    #fname = "GIPSVoiceEngineDLL_MD.dll"
-
-    absPath = os.path.abspath(fname)
-
+def parse_tables(absPath):
+    f = open(absPath + ".out", 'w')
     print "file: " + absPath
     pe = pefile.PE(absPath)
-
     print "\n----Import table----"
+    f.write("---Import table---\n")
+
     for entry in pe.DIRECTORY_ENTRY_IMPORT:
         print entry.dll
         for imp in entry.imports:
             print '\t', imp.name
-
+            f.write(entry.dll + ","+ imp.name +"\n")
 
     print "\n----Export table----"
+    f.write("----Export table----\n")
     for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
         print '\t', exp.name
+        f.write(exp.name +"\n")
+
+    f.close()
+
+def main():
+    fname = sys.argv[1]
+    absPath = os.path.abspath(fname)
+    parse_tables(absPath)
+
 
 if __name__ == "__main__":
     main()
